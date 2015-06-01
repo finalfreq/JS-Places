5cb94e4c1f56c53c447b1cfffbb32934521e427d
Ian <ian@iancmacdonald.com>
2015-06-01 12:35:30 -0700

    Add form with datepicker for year.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 "use strict";
 
-
-//jQuery
-$( document ).ready(function() {
-    console.log( "jQuery Ready" );
-
-    $("#jqtest").text('jQuery Ready')
+// Places {name: location: season: year: description: x}
+$(document).ready(function(){
+  $(".input-group.date").datepicker({
+      format: "yyyy", // Notice the Extra space at the beginning
+      // viewMode: "years",
+      minViewMode: 2
+  });
 });
-
-//raw js
-var foo = function(bar) {
-  return false;
-};
